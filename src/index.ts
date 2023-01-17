7c96c8a4076874ff175bf6efc9414be42dd7dd0f
VASAPOL RITTIDEAH <70092356+vasapol123@users.noreply.github.com>
2023-01-17 18:56:27 +0700

    feat: get row w/ populated merge cells

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 /* eslint-disable import/extensions */
+import { PrismaClient } from '@prisma/client';
 // eslint-disable-next-line import/no-unresolved
 import SpreadSheetHandler from './spreadsheet.js';
 
 const spreadsheetHandler = new SpreadSheetHandler(
   <string>process.env.SPREADSHEET_ID
 );
+const prisma = new PrismaClient();
 
-console.log(
-  await spreadsheetHandler.populateMergedCell('Grade', 'GPA')
-);
+console.log(await spreadsheetHandler.getRow('6301012620171', 42));
--- src/spreadsheet.ts
+++ src/spreadsheet.ts
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
 /* eslint-disable camelcase */
 import { google, sheets_v4, Auth } from 'googleapis';
+import { GaxiosError } from 'gaxios';
 import * as dotenv from 'dotenv';
 
 dotenv.config();
@@ -31,14 +32,14 @@ export default class SpreadSheetHandler {
     this.spreadsheetId = spreadsheetId;
   }
 
-  public async getHeader(sheet_name: string): Promise<string[] | undefined> {
+  public async getHeader(sheet_name: string): Promise<string[]> {
     const response = await this.googleSheets.spreadsheets.values.get({
       auth: this.googleAuth,
       spreadsheetId: this.spreadsheetId,
       range: `${sheet_name}!A1:H1`,
     });
 
-    return response.data.values?.[0];
+    return response.data.values![0];
   }
 
   public async getCell(sheet_name: string, value: string) {
@@ -51,23 +52,62 @@ export default class SpreadSheetHandler {
     return response.data.values?.[0];
   }
 
-  public async populateMergedCell(
+  public async getRow(sheet_name: string, num: number): Promise<string[]> {
+    try {
+      const response = await this.googleSheets.spreadsheets.values.get({
+        auth: this.googleAuth,
+        spreadsheetId: this.spreadsheetId,
+        range: `${sheet_name}!A${num}:H${num}`,
+      });
+
+      const unpopulatedRow = <string[]>response.data.values![0];
+
+      const populatedRow = await Promise.all(
+        (<(keyof typeof MergedCell | string)[]>(
+          await this.getHeader(sheet_name)
+        )).map(async (curr, i) => {
+          if (
+            (<string[]>Object.keys(MergedCell)).includes(curr.toUpperCase())
+          ) {
+            const col = await this.populateMergedCol(
+              sheet_name,
+              <keyof typeof MergedCell>curr.toUpperCase()
+            );
+            if (!unpopulatedRow[i]) {
+              return col[num][0] || col[col.length - 1][0];
+            }
+          }
+          return unpopulatedRow[i];
+        })
+      );
+
+      return populatedRow;
+    } catch (e) {
+      throw Error((<GaxiosError>e).response?.statusText);
+    }
+  }
+
+  private async populateMergedCol(
     sheet_name: string,
     value: keyof typeof MergedCell
-  ) {
-    const range = `${sheet_name}!${MergedCell[value]}:${MergedCell[value]}`;
+  ): Promise<string[][]> {
+    try {
+      const range = `${sheet_name}!${MergedCell[value]}:${MergedCell[value]}`;
 
-    const response = await this.googleSheets.spreadsheets.values.get({
-      auth: this.googleAuth,
-      spreadsheetId: this.spreadsheetId,
-      range,
-    });
+      const response = await this.googleSheets.spreadsheets.values.get({
+        auth: this.googleAuth,
+        spreadsheetId: this.spreadsheetId,
+        range,
+      });
 
-    (response.data.values! as string[][]).map((curr, i, arr) => {
-      if (!curr.length) arr[i].push(arr[i - 1][0]);
-      return curr;
-    });
+      (response.data.values! as string[][]).map((curr, i, arr) => {
+        if (!curr.length) arr[i].push(arr[i - 1][0]);
+        return curr;
+      });
 
-    return response.data.values;
+      return response.data.values!;
+    } catch (e) {
+      throw Error((<GaxiosError>e).response?.statusText);
+    }
   }
 }
