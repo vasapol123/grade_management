4131990c25fe2ae1dd354ad7ebb97a9d1ee75cf1
VASAPOL RITTIDEAH <70092356+vasapol123@users.noreply.github.com>
2023-01-19 21:41:19 +0700

    feat: use redis to store temp data

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 /* eslint-disable import/no-unresolved */
 // import { studentCodeToYear } from './spreadsheet/spreadsheet.helper.js';
+import redis from 'redis';
 import SpreadsheetHelper from './spreadsheet/spreadsheet.helper.js';
 import DatabaseHandler from './database.handler.js';
 import SpreadsheetHandler from './spreadsheet/spreadsheet.handler.js';
@@ -9,7 +10,19 @@ const spreadsheetHelper = new SpreadsheetHelper();
 const databaseHandler = new DatabaseHandler();
 
 const sheets = await spreadsheetHandler.getSheets();
+const sheetTitles = sheets.map((curr) => {
+  return <string>curr.properties?.title;
+});
 
 // databaseHandler.createStudents(
 //   sheets.map((curr) => { year: studentCodeToYear(<string>curr.properties?.title) })
 // );
+
+const client = redis.createClient({
+  url: process.env.REDIS_URL,
+});
+
+client.connect().then(async () => {
+  console.log(await client.get('key'));
+  client.quit();
+});
